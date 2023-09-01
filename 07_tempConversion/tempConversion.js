604adfaf54cb80766a2b803ef8026ee82e8da2b2
Santiago <sacifuentespawk@gmail.com>
2023-08-31 21:49:01 -0500

    temperatures conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  celsius = ((temperature)-32)/1.8;
+  return +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  fahrenheit = (temperature*1.8)+32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
