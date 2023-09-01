8f113a356e7e630cd9f733e72a0b65a18f34ab33
Santiago <sacifuentespawk@gmail.com>
2023-09-01 17:19:48 -0500

    palindrome check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const reverseString = function (string) {
+    let reversedString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i]; 
+    }
+    return reversedString;
 };
+
+// Do not edit below this line
+module.exports = reverseString;
+
+
+const palindromes = function (word) {
+    word = word.toLowerCase().replaceAll(' ','').replaceAll('.', '').replaceAll(',','').replaceAll('!','');
+    reversedString = reverseString(word);
+    if (word === reversedString){
+        return true;
+    }
+    return false;
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
