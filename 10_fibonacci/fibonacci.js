5a106bd09b04edd110c67ea477d03517e5ed8291
Santiago <sacifuentespawk@gmail.com>
2023-09-01 17:40:30 -0500

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (numberCheck) {
+    if (numberCheck < 1) {
+        return 'OOPS'
+    }
 
+    let oldNumber = 1;
+    let newNumber = 1;
+    let fibonacciNumber = 1;
+
+    for (let i = 2; i < numberCheck; ++i) {
+        fibonacciNumber = oldNumber + newNumber;
+        oldNumber = newNumber;
+        newNumber = fibonacciNumber;
+    }
+    return fibonacciNumber;
 };
 
+console.log(fibonacci(10))
 // Do not edit below this line
 module.exports = fibonacci;
