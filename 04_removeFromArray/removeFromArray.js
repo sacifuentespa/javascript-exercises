050423f2b9065d492dda2ca0be882a253af3fe27
Santiago <sacifuentespawk@gmail.com>
2023-08-31 20:39:00 -0500

    remove exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let originArray = arguments[0];
+    let toRemove = Array.from(arguments).slice(1);
 
+    for (let i = 0; i < toRemove.length; i++) {
+        if (originArray.indexOf(toRemove[i]) !== -1) {
+            originArray.splice(originArray.indexOf(toRemove[i]), 1)
+        }
+    }
+    return originArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
