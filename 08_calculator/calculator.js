77bdb3d7a05f45212019e2c0fa72bbb05fc04358
Santiago <sacifuentespawk@gmail.com>
2023-08-31 22:08:00 -0500

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNumber, secondNumber) {
+  return firstNumber - secondNumber
 };
 
-const sum = function() {
-	
+const sum = function (sumArray) {
+  let total = 0;
+
+  for (let i = 0; i < sumArray.length; i++) {
+    total += sumArray[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function (multArray) {
+  let total = 0;
+
+  for (let i = 0; i < multArray.length; i++) {
+    if(total === 0){
+      total = multArray[i];
+    }else{
+      total *= multArray[i];
+    }
+  }
+
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function (firstNumber,secondNumber) {
+  return firstNumber**secondNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function (factorNumber) {
+  let resultFactorial = 1;
+  if(factorNumber === 0 || factorNumber === 1){
+    return 1;
+  }
+  else{
+    for(let i=1;i<=factorNumber;i++){
+      resultFactorial *= i; 
+    }
+  }
+  return resultFactorial;
 };
 
 // Do not edit below this line
