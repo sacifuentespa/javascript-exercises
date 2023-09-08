a0102d031a4e77a0f1e008f86bd72c9c89cce55a
Santiago <sacifuentespawk@gmail.com>
2023-09-08 15:13:46 -0500

    11th exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obArray) {
+    returnArray = [];
+    for(let i=0;i<obArray.length;i++){
+        returnArray.push(obArray[i].title);
+    }
+    return returnArray
 };
 
 // Do not edit below this line
